ff0ce5a5b7663d12ad60a00d3a374e971a6aab46
Ethan Duvall <eduvall23@turing.edu>
2024-02-08 01:56:58 -0700

    feature/fetch-api

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,48 +1,50 @@
 //NOTE: Your DOM manipulation will occur in this file
-import usersData from "./data/users";
+import { fetchHydrationData, fetchUserData } from "./apiCalls";
+
 import { CircularFluidMeter } from "fluid-meter";
-import hydration from "./data/hydration"
-import {
-  getUserData,
-  getAverageSteps
-} from "./users";
+import { getUserData, getAverageSteps } from "./users";
 import {
   getFluidOunceForDay,
   getFluidOunceForWeek,
-  getAverageFluidOunce
+  getAverageFluidOunce,
 } from "./hydration";
 
 const info = document.querySelector("#info");
 const friendsList = document.querySelector("#friends");
-const waterMeter = document.querySelector("#waterMeterContainer"); 
+const waterMeter = document.querySelector("#waterMeterContainer");
 const userHydrationDate = document.querySelector("#dateHydrationTitle");
 const hydrationWeekButtons = document.querySelector("#hydrationDays");
-
-
-
-const randomIndex = Math.floor(Math.random() * (usersData.users.length - 1)) + 1;
 const randomDay = Math.floor(Math.random() * 100);
-const user = getUserData(randomIndex, usersData.users);
-const flOzDays = getFluidOunceForWeek(user.id, hydration.hydrationData);
+let user
+Promise.all([fetchHydrationData(), fetchUserData()])
+.then(([hydration, usersData]) => {
+  const randomIndex =
+    Math.floor(Math.random() * (usersData.users.length - 1)) + 1;
+  user = getUserData(randomIndex, usersData.users);
+  let avgStep = getAverageSteps(usersData.users);
+  const flOzDays = getFluidOunceForWeek(user.id,hydration.hydrationData);
+  updateUserInfo(avgStep);
+  updateHydration(randomDay,hydration,flOzDays);
+});
+
 let createdWaterMeter = new CircularFluidMeter(waterMeter, {
   borderWidth: 15,
   maxProgress: 100,
   initialProgress: 0,
-  backgroundColor: '#002d59',
-  borderColor: '#3e4954',
-  bubbleColor: '#6bcfff',
-  fontFamily: 'Codystar',
+  backgroundColor: "#002d59",
+  borderColor: "#3e4954",
+  bubbleColor: "#6bcfff",
+  fontFamily: "Codystar",
   fontSize: 34,
   progressFormatter: (value) => {
-      return `${value.toFixed(0)} fl oz`;
+    return `${value.toFixed(0)} fl oz`;
   },
   fluidConfiguration: {
-      color: '#1e90ff'
-  }
-})
+    color: "#1e90ff",
+  },
+});
 
-function updateUserInfo() {
-  let avgStep = getAverageSteps(usersData.users)
+function updateUserInfo(avgStep) {
   info.innerHTML = `<h1 id="name">Welcome: ${user.name}</h1>
   <h3 id="userID">ID: ${user.id} </h3>
   <h3 id="address">Address: ${user.address} </h3>
@@ -51,14 +53,13 @@ function updateUserInfo() {
   <h3 id="stepGoal">My Step Goal: ${user.dailyStepGoal} steps</h3>
   <h3 id="comparedStepGoal"> Avg Step Goal: ${avgStep} steps`;
   updateFriendsList(user.friends);
-  updateHydration(user, randomDay);
 }
 
 function updateFriendsList(friends) {
   friends.forEach((friend) => {
     friendsList.insertAdjacentHTML(
       "beforeend",
-        `<aside>
+      `<aside>
         <h3>${friend.name}</h3>
         <h3>Step Goal: ${friend.dailyStepGoal}</h3>
         </aside>`
@@ -66,23 +67,24 @@ function updateFriendsList(friends) {
   });
 }
 
-function updateHydration(user, day = 0) {
-  const userHydration = getFluidOunceForDay(user.id, hydration.hydrationData, flOzDays[day].date);
+function updateHydration(day = 0, hydration,flOzDays) {
+  const userHydration = getFluidOunceForDay(
+    user.id,
+    hydration.hydrationData,
+    flOzDays[day].date
+  );
   userHydrationDate.innerHTML = `<h1>Water Consumption</h1><h3>${userHydration.date}</h3>`;
-  createdWaterMeter.progress = userHydration.numOunces
+  createdWaterMeter.progress = userHydration.numOunces;
 }
 
-window.addEventListener("load", function () {
-  updateUserInfo();
-});
 
 hydrationWeekButtons.addEventListener("click", (event) => {
-  if(!flOzDays[(randomDay - Number(event.target.id))]) {
+  if (!flOzDays[randomDay - Number(event.target.id)]) {
     userHydrationDate.innerHTML = `<h1>No Data to Display...`;
     createdWaterMeter.progress = 0;
   } else {
-    updateHydration(user, (randomDay - Number(event.target.id)));
+    updateHydration(user, randomDay - Number(event.target.id));
   }
-})
+});
 
 export { updateUserInfo };
