682c4819db8f497b55d81797ccd668bc3d65a44f
David Kwon <dkwon1223@gmail.com>
2024-02-08 20:45:32 -0700

    Implement functions in sleep.js and fetch call for sleep data in apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,14 +1,25 @@
 // Your fetch requests will live here!
-(fetch("https://fitlit-api.herokuapp.com/api/v1/users").then(response => response.json()).then(data => usersData = data))
+fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+  .then((response) => response.json())
+  .then((data) => (usersData = data));
 
-let usersData 
-let hydration
-function fetchUserData(){
-    return (fetch("https://fitlit-api.herokuapp.com/api/v1/users").then(response => response.json()).then(data => usersData = data))
+let usersData, hydration, sleep;
+
+function fetchUserData() {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+    .then((response) => response.json())
+    .then((data) => (usersData = data));
 }
-function fetchHydrationData(){
-    return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration").then(repsonse => repsonse.json()).then(data => hydration = data)
+function fetchHydrationData() {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+    .then((repsonse) => repsonse.json())
+    .then((data) => (hydration = data));
 }
 
+function fetchSleepData() {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+    .then((repsonse) => repsonse.json())
+    .then((data) => (sleep = data));
+}
 
-export{ fetchHydrationData,fetchUserData, usersData, hydration}
+export { fetchHydrationData, fetchUserData, usersData, hydration, sleep };
