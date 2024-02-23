8eb10df76adb707da8f68eef76f77a6c3a92b7ca
Cory Sanders <corysanders3@yahoo.com>
2024-02-22 22:45:07 -0700

    Add functionality for the user to add in hydration data.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,38 @@
 function fetchUserData(usersData) {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+  return fetch("http://localhost:3001/api/v1/users")
     .then((response) => response.json())
     .then((data) => (usersData = data));
 }
 function fetchHydrationData(hydration) {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+  return fetch("http://localhost:3001/api/v1/hydration")
     .then((repsonse) => repsonse.json())
     .then((data) => (hydration = data));
 }
 
 function fetchSleepData(sleep) {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+  return fetch("http://localhost:3001/api/v1/sleep")
     .then((repsonse) => repsonse.json())
     .then((data) => (sleep = data));
 }
 
-export { fetchHydrationData, fetchUserData, fetchSleepData};
+function postHydrationData(date, numOunces, userID) {
+  fetch("http://localhost:3001/api/v1/hydration", {
+    method: 'POST',
+    body: JSON.stringify({
+      date,
+      numOunces,
+      userID
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then(response => {
+    if(!response.ok) {
+      throw new Error("There was an issue adding your hydration details. Please try again later.")
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => popUpError.innerText = error.message)
+}
+
+export { fetchHydrationData, fetchUserData, fetchSleepData, postHydrationData };
